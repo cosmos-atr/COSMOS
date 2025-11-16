ed5538da28979560594de8dfbb083102c8f57ea0
cosmos-atr <cosmosartlab@gmail.com>
2025-11-16 20:23:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const card = document.querySelector(".mirror-card");
+    const burger = document.getElementById("burger");
+    const nav = document.getElementById("nav-links");
 
+    burger.addEventListener("click", () => {
+        nav.classList.toggle("show");
+    });
+
+    const card = document.querySelector(".mirror-card");
     setTimeout(() => {
         card.style.opacity = "1";
         card.style.transform = "translateY(0)";
