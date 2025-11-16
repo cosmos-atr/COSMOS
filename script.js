9d49aee843ce96521758ca1e7d03972c6c9ba700
cosmos-atr <cosmosartlab@gmail.com>
2025-11-16 20:10:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,22 +5,5 @@ document.addEventListener('DOMContentLoaded', function() {
         mirrorCard.style.transition = 'opacity 0.8s ease, transform 0.8s ease';
         mirrorCard.style.opacity = '1';
         mirrorCard.style.transform = 'translateY(0)';
-
-        const cardTitle = mirrorCard.querySelector('h2');
-        const cardParagraphs = mirrorCard.querySelectorAll('p');
-
-        setTimeout(() => {
-            cardTitle.style.transition = 'opacity 0.6s ease, transform 0.6s ease';
-            cardTitle.style.opacity = '1';
-            cardTitle.style.transform = 'translateX(0)';
-        }, 300);
-
-        cardParagraphs.forEach((p, index) => {
-            setTimeout(() => {
-                p.style.transition = 'opacity 0.6s ease, transform 0.6s ease';
-                p.style.opacity = '1';
-                p.style.transform = 'translateX(0)';
-            }, 500 + index * 200);
-        });
-    }, 1200);
+    }, 800);
 });
